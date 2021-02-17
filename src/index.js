1db8ee4eb406a8a4e8b40bfed01def71601293a8
winstonbain <winston.feng@bain.com>
2021-02-16 20:45:18 -0600

    1.6 unicafe

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+  }
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text='good' />
+      <Button onClick={handleNeutral} text='neutral' />
+      <Button onClick={handleBad} text='bad' />
+
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
