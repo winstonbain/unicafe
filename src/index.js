06eb053939a4da29a6b6020ab1bb15aaee1ed8f0
winstonbain <winston.feng@bain.com>
2021-02-25 15:25:32 -0600

    Exercise 1.8 complete

--- src/index.js
+++ src/index.js
@@ -12,16 +12,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, calcTotal] = useState(0)
+  const [all, calcAll] = useState(0)
 
   const handleGood = () => {
     setGood(good+1)
+    calcTotal(total+1)
+    calcAll(all+1)
   }
   const handleNeutral = () => {
     setNeutral(neutral+1)
+    calcAll(all+1)
   }
   const handleBad = () => {
     setBad(bad+1)
+    calcTotal(total-1)
+    calcAll(all+1)
   }
+
+  const Statistics = (props) => {
+    return(
+      <div>
+      <p>All {props.all}</p>
+      <p>Average {(props.total/props.all)}</p>
+      <p>Positive {props.good/props.all} %</p>
+      </div>
+    )
+
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -33,6 +52,7 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <Statistics total={total} all={all} good={good}/>
     </div>
   )
 }
